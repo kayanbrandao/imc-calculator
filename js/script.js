6e548eb9b10abcbe05077fe7bd520eeab52dacb9
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-10-22 16:40:45 -0400

    Atualizando projeto

--- js/script.js
+++ js/script.js
@@ -3,15 +3,6 @@ const inputHeight = document.querySelector("#input-height");
 const inputWeight = document.querySelector("#input-weight");
 const result = document.querySelector(".result");
 
-btnCalculate.addEventListener("click", () => {
-	let weight = Number(inputWeight.value.replace(",", "."));
-	let height = Number(inputHeight.value.replace(",", "."));
-	let imc = weight / (height * height);
-	imc = imc.toFixed(1).replace(".", ",");
-
-	result.innerHTML = imc;
-});
-
 const inputWeightPattern = {
 	mask: "000{,}00",
 };
@@ -22,3 +13,28 @@ const inputHeightPattern = {
 
 const inputWeightMasked = IMask(inputWeight, inputWeightPattern);
 const inputHeightMasked = IMask(inputHeight, inputHeightPattern);
+
+const checkFields = (weight, height) => {
+	if (weight === "" || height === "") {
+		alert("Preencha todos os campos!");
+		return true;
+	}
+};
+
+const calculateImc = (weight, height) => {
+	let weightNumber = Number(weight.replace(",", "."));
+	let heightNumber = Number(height.replace(",", "."));
+	let imc = weightNumber / (heightNumber * heightNumber);
+	imc = imc.toFixed(1).replace(".", ",");
+	return imc;
+};
+
+btnCalculate.addEventListener("click", () => {
+	let weight = inputWeight.value;
+	let height = inputHeight.value;
+
+	if (!checkFields(weight, height)) {
+		let imc = calculateImc(weight, height);
+		result.innerHTML = imc;
+	}
+});
